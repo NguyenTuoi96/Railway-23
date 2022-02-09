d18aece0b8539a5aac5557537bfd14122d55598d
NguyenTuoi96 <tuoinguyen01111996@gmail.com>
2022-02-09 22:04:20 +0900

    final exam

--- Frontend Basic/FrontEnd–FinalExam/javascript/custom.js
+++ Frontend Basic/FrontEnd–FinalExam/javascript/custom.js
@@ -1,137 +1,112 @@
-// set tên nội dung
-document.getElementById("contentName").innerText = "List Group";
+// const
+const tableHeader = "<table><tr><th style='width: 5%'></th><th style='width: 5%'>STT</th><th style='width: 25%'>Group Name</th><th style='width: 20%'>Member</th><th style='width: 25%'>Cretor</th><th style='width: 20%'>Create Date</th></tr>";
+const apiUrl = "https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group";
+const sysErrMsg = "An error occurred, please check again!";
+const duplicateErrMsg = "Group name is exits!";
 
+// set tên nội dung
+$("#contentName").text("List Group");
+
+// ẩn phần chi tiết
 $("#detail").hide();
 
+// lấy dữ liệu
 getList();
 
-function getList(){
-    document.getElementById("listTable").innerHTML = "";
-
+// hàm lấy dữ liệu
+function getList(){    
+    $("#list").show();
+    $("#detail").hide();
+    $("#search").val("");
+    $("#listTable").html("");
     $.ajax({
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group',
-        type: 'GET',
+        url: apiUrl,
+        type: "GET",
         success:function(result){
-            str = document.getElementById("listTable").innerHTML;
-            str = str.concat("<table><tr><th style='width: 5%'></th><th style='width: 5%'>STT</th><th style='width: 25%'>Group Name</th><th style='width: 20%'>Member</th><th style='width: 25%'>Cretor</th><th style='width: 20%'>Create Date</th></tr>");
+            var str = "";
+            str = str.concat(tableHeader);
             cnt = 0;
             result.forEach(element => {
                 cnt++;
                 str = str.concat("<tr><td style='text-align: center'><input type='checkbox' name='checkRow' id='", element.id ,"'></td>"
                             ,"<td style='text-align: center'>", cnt ,"</td>"
-                            ,"<td><p class='groupName' onclick='showDetail(", element.id ,")'>", element.groupName ,"</p></td>"
+                            ,"<td><a href='#' class='groupName' onclick='showDetail(", element.id ,")'>", element.groupName ,"</a></td>"
                             ,"<td style='text-align: center'>", element.memberNum ,"</td>"
                             ,"<td>", element.creator.name == undefined ? '' : element.creator.name ,"</td>"
                             ,"<td style='text-align: center'>", element.createDate.substring(0, 10) ,"</td>"                        
                             ,"</tr>");
             });
             str.concat("</table>")
-            document.getElementById("listTable").innerHTML = str;
+            $("#listTable").html(str);
         }
     })
 }
 
-function search(){
-    document.getElementById("listTable").innerHTML = "";
-    var id = document.getElementById("search").value;
-
-    $.ajax({
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group/' + id,
-        type: 'GET',
-        success:function(result){
-            str = document.getElementById("listTable").innerHTML;
-            str = str.concat("<table><tr><th style='width: 5%'></th><th style='width: 5%'>STT</th><th style='width: 25%'>Group Name</th><th style='width: 20%'>Member</th><th style='width: 25%'>Cretor</th><th style='width: 20%'>Create Date</th></tr>");
-            cnt = 1;
-            str = str.concat("<tr><td style='text-align: center'><input type='checkbox' name='checkRow' id='", result.id ,"'></td>"
-                            ,"<td style='text-align: center'>", cnt ,"</td>"
-                            ,"<td><p class='groupName' onclick='showDetail(", result.id ,")'>", result.groupName ,"</p></td>"
-                            ,"<td style='text-align: center'>", result.memberNum ,"</td>"
-                            ,"<td>", result.creator.name == undefined ? '' : result.creator.name ,"</td>"
-                            ,"<td style='text-align: center'>", result.createDate.substring(0, 10) ,"</td>"                        
-                            ,"</tr>");
-            str.concat("</table>")
-            document.getElementById("listTable").innerHTML = str;
-        }
-    })
-}
-
-function showCreateModal(){
-    var modal = document.getElementById("createGroupModal");
-    modal.style.display = "block";
-}
-
-function closeCreateGroupModal(){
-    resetModalCreate();
-    var modal = document.getElementById("createGroupModal");
-    modal.style.display = "none";
-}
-
-function checkCreateGroup(){
-    // lấy groupName
-    var groupName = document.getElementById("inputGroupName").value;
-    var check = true;
-
-    $.ajax({
-        async: false,
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group',
-        type: 'GET',
-        success:function(result){
-            result.forEach(element => {
-                if(element.groupName == groupName){
-                    document.getElementById("errInfoCreate").innerText = "Group name is exits!";
-                    check = false;
-                }
-            });
-        }
-    })
-
-    if(check){
-        createGroup();
-    }
-}
-
-function createGroup(){
-    // lấy groupName
-    var groupName = document.getElementById("inputGroupName").value;
-
-    // khai báo đối tượng
-    var group = {
-        "groupName": groupName,
-        "memberNum": 11,
-        "creator": {
-         "name": "Nguyen A",
-         "id": "1"
-        },
-        "createDate": "2022-02-05T13:46:04.851Z"
-    }
-
-    $.ajax({
-        async: false,
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group',
-        type: 'POST',
-        data: group,
-        success:function(result){
-            if(result==undefined||result==null){
-                alert("Có lỗi xảy ra");
-                return;
-            }else{
-                alert("Thêm group thành công");
+// hàm lấy dữ liệu theo id
+function searchById(){
+    var id = $("#search").val();
+    if(id != ""){
+        $("#listTable").html("");
+        $.ajax({
+            url: apiUrl + '/' + id,
+            type: 'GET',
+            success:function(result){
+                var str = "";
+                str = str.concat(tableHeader);
+                cnt = 1;
+                str = str.concat("<tr><td style='text-align: center'><input type='checkbox' name='checkRow' id='", result.id ,"'></td>"
+                                ,"<td style='text-align: center'>", cnt ,"</td>"
+                                ,"<td><a class='groupName' onclick='showDetail(", result.id ,")'>", result.groupName ,"</a></td>"
+                                ,"<td style='text-align: center'>", result.memberNum ,"</td>"
+                                ,"<td>", result.creator.name == undefined ? '' : result.creator.name ,"</td>"
+                                ,"<td style='text-align: center'>", result.createDate.substring(0, 10) ,"</td>"                        
+                                ,"</tr>");
+                str.concat("</table>")
+                $("#listTable").html(str);
             }
-        }
-    })
-    closeCreateGroupModal();
-    getList();
+        })
+    }else{
+        getList();
+    }
 }
 
+// show modal thêm group
+function showCreateGroupModal(){
+    $("#groupId").text("");
+    $("#modalName").text("Create Group");
+    $("#groupEditModal").show("slow");
+    $("#inputGroupName").focus();
+}
+
+// show modal sửa group
+function showEditGroupModal(id){    
+    $("#groupId").text(id);
+    $("#modalName").text("Edit Group");
+    $("#groupEditModal").show("slow");
+    $("#inputGroupName").focus();
+}
+
+// đóng modal thêm/sửa group
+function closeGroupModal(){
+    $("#inputGroupName").val("");
+    $("#errInfo").text("");
+    $("#groupId").text("");
+
+    $("#groupEditModal").hide("slow");
+}
+
+// show detail
 function showDetail(id){
     $.ajax({
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group/' + id,
+        url: apiUrl + "/" + id,
         type: 'GET',
         success:function(result){
-            document.getElementById("groupDetailHeader").innerHTML = "<h3 id='groupName'>" + result.groupName + "<i class='far fa-pencil-alt pencil' onclick='showEditModal(" + id + ")'></i>" + "</h3>";
-            document.getElementById("creatorName").innerText = result.creator.name == undefined ? '' : result.creator.name;
-            document.getElementById("createDate").innerText = result.createDate.substring(0, 10);
-            document.getElementById("member").innerText = result.memberNum;
+            $("#groupDetailHeader").html(
+                "<h3 id='groupName'>" + result.groupName + "<i class='far fa-pencil-alt pencil' onclick='showEditGroupModal(" + id + ")'></i>" + "</h3>"
+            );
+            $("#creatorName").text(result.creator.name == undefined ? '' : result.creator.name);
+            $("#createDate").text(result.createDate.substring(0, 10));
+            $("#member").text(result.memberNum);
         }
     })
 
@@ -139,94 +114,132 @@ function showDetail(id){
     $("#detail").show();
 }
 
-function showEditModal(id){    
-    document.getElementById("groupId").innerText = id;
-    var modal = document.getElementById("editGroupModal");
-    modal.style.display = "block";
-}
-
-function closeEditGroupModal(){
-    resetModalEdit();
-    var modal = document.getElementById("editGroupModal");
-    modal.style.display = "none";
-}
-
-function checkEditGroup(){
-    // lấy groupName
-    var groupName = document.getElementById("inputEditGroupName").value;
+// kiểm tra trùng tên group khi thêm/sửa
+function checkGroupNameDuplication(groupName){
     var check = true;
-
     $.ajax({
         async: false,
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group',
+        url: apiUrl,
         type: 'GET',
         success:function(result){
             result.forEach(element => {
                 if(element.groupName == groupName){
-                    document.getElementById("errInfoEdit").innerText = "Group name is exits!";
                     check = false;
                 }
             });
         }
     })
 
-    if(check){
-        editGroup();
+    return check;
+}
+
+// event của btton save
+$("#btnSave").click(function(){
+    var groupName = $("#inputGroupName").val();
+    var check = checkGroupNameDuplication(groupName);
+    if(check == false){
+        $("#errInfo").text(duplicateErrMsg);
+    }else{
+        var id = document.getElementById("groupId").textContent;
+        if(id != ""){
+            // Update
+            editGroup(id, groupName);
+        }else{
+            // Create
+            createGroup(groupName);
+        }
+    }
+});
+
+function saveGroup(){
+    var groupName = $("#inputGroupName").val();
+    var check = checkGroupNameDuplication(groupName);
+    if(check == false){
+        $("#errInfo").text(duplicateErrMsg);
+    }else{
+        var id = document.getElementById("groupId").textContent;
+        if(id != ""){
+            // Update
+            editGroup(id, groupName);
+        }else{
+            // Create
+            createGroup(groupName);
+        }
     }
 }
 
-function editGroup(){
-    // lấy group id
-    var id = document.getElementById('groupId').textContent;
-    // lấy groupName
-    var groupName = document.getElementById("inputEditGroupName").value;    
-
+// Thêm group
+function createGroup(groupName){
+    var now = new Date();
     // khai báo đối tượng
     var group = {
         "groupName": groupName,
-        "memberNum": 30,
+        "memberNum": 0,
         "creator": {
             "name": "Nguyen A",
             "id": "1"
         },
-        "createDate": "2022-02-05T11:31:14.525Z",
-        "id": "6"
+        "createDate": now.toISOString()
     }
 
     $.ajax({
         async: false,
-        url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group/' + id,
+        url: apiUrl,
+        type: 'POST',
+        contentType:"application/json;charset=utf-8",
+        data: JSON.stringify(group),
+        success:function(result){
+            if(result == undefined || result == null){
+                showInfomation(true, sysErrMsg)
+                return;
+            }else{
+                showInfomation(false, "Add group success")
+            }
+        }
+    })
+    closeGroupModal();
+    getList();
+}
+
+// Sửa group
+function editGroup(id, groupName){
+    // khai báo đối tượng
+    var group = {
+        groupName: groupName
+    }
+
+    $.ajax({
+        async: false,
+        url: apiUrl + "/" + id,
         type: 'PUT',
         data: group,
         success:function(result){
             if(result == undefined || result == null){
-                alert("Có lỗi xảy ra");
+                showInfomation(true, sysErrMsg)
                 return;
             }else{
-                alert("Sửa group thành công");
+                showInfomation(false, "Update group success")
             }
         }
     })
 
-    closeEditGroupModal();
+    closeGroupModal();
     $("#list").show();
     $("#detail").hide();
     getList();
-
 }
 
+// xóa group
 function showDelConfirm(){
     if($('input[name="checkRow"]:checked').length == 0){
-        alert("Hãy chọn group");
+        showInfomation(true, "Hãy chọn group!")
         return;
     }
-    var modal = document.getElementById("deleteGroupModal");
-    modal.style.display = "block";
+    $("#deleteGroupModal").show("slow");
 }
 
 function closeDeleteGroupModal(){
-    var modal = document.getElementById("deleteGroupModal");
-    modal.style.display = "none";
+    $("#deleteGroupModal").hide("slow");
 }
 
 function deleteGroup(){
@@ -234,7 +247,7 @@ function deleteGroup(){
     $('input[name="checkRow"]:checked').each(function() {
         $.ajax({
             async: false,
-            url: 'https://61ed2685f3011500174d22e6.mockapi.io/api/v1/group/' + this.id,
+            url: apiUrl + "/" + this.id,
             type: 'DELETE',
             data: null,
             success:function(result){
@@ -246,23 +259,38 @@ function deleteGroup(){
      });
 
     if(checkDel == false){
-        alert("Có lỗi xảy ra");
+        showInfomation(true, sysErrMsg)
     }else{
-        alert("Xóa group thành công");
+        showInfomation(false, "Delete group success")
     }
 
     closeDeleteGroupModal();
     getList();
 }
 
-function resetModalEdit(){
-    document.getElementById("inputEditGroupName").value = "";
-    document.getElementById("errInfoEdit").innerText = "";
-    document.getElementById("groupId").innerText = "";
+// show Infomation
+function showInfomation(isErr, infoMsg){
+    if(isErr){
+        $("#infoName").html(
+            "<h3 id='infoName'><i class='fas fa-times-circle errInfoIcon'></i> Error</h3>"
+        );
+    }else{
+        $("#infoName").html(
+            "<h3 id='infoName'><i class='fas fa-check-circle infoIcon'></i> Info</h3>"
+        );
+    }
+    
+    $("#infoContent").text(infoMsg);
+
+    $("#infoMsg").show();
 }
 
-function resetModalCreate(){
-    document.getElementById("inputGroupName").value = "";
-    document.getElementById("errInfoCreate").innerText = "";
-}
+// hide Infomation
+function closeInfoMsg(){
+    $("#infoName").html(
+        "<h3 id='infoName'></h3>"
+    );    
+    $("#infoContent").text("");
 
+    $("#infoMsg").hide();
+}
